d6ab73fc9e8a08c2c30bbffda18a9a75885332bd
Daniel Trnka <daniel.trnka@gmail.com>
2024-09-01 20:07:16 +0200

    dump: added frame/node filtering

--- plintools/dump.py
+++ plintools/dump.py
@@ -6,12 +6,27 @@ import os
 from rich import print
 from rich.table import Table
 from rich.markup import escape
+from rich.console import Console
+
+stderr = Console(stderr=True, style="red")
 
 
 class DumpCommand:
     def parser(self, parser: argparse.ArgumentParser):
         parser.add_argument("ldf_path")
         parser.add_argument("device")
+        parser.add_argument(
+            "--frame",
+            "-f",
+            action="append",
+            help="Show only this frame identified by its ID or name",
+        )
+        parser.add_argument(
+            "--master", "-m", action="store_true", help="Show master frames only"
+        )
+        parser.add_argument(
+            "--node", "-n", action="append", help="Show frames from this node"
+        )
 
     def get_longest_signal(self, ldf: ldfparser.LDF) -> int:
         max_found = 0
@@ -27,6 +42,42 @@ class DumpCommand:
                     max_found = max(max_found, len(converter.info))
         return max_found
 
+    def get_allowed_frames(self, ldf: ldfparser.LDF, frames, master, nodes):
+        nodes = set(nodes or [])
+        all_nodes = set([node.name for node in ldf.slaves] + [ldf.master.name])
+        if nodes - all_nodes:
+            stderr.print("Unknown nodes:", ", ".join(nodes - all_nodes))
+            stderr.print("Available nodes are: ")
+            stderr.print("\n".join(sorted(all_nodes - nodes)))
+            exit(1)
+        if master:
+            nodes.add(ldf.master.name)
+
+        requested_frame_ids = []
+        if frames:
+            for frame in frames:
+                if frame.startswith("0x"):
+                    frame = int(frame, 16)
+                try:
+                    requested_frame_ids.append(ldf.get_frame(frame).frame_id)
+                except LookupError:
+                    stderr.print(f"Frame {frame} not found.")
+                    stderr.print("Available frames:")
+                    stderr.print("\n".join([frame.name for frame in ldf.frames]))
+                    exit(1)
+
+        def is_frame_allowed(frame: ldfparser.LinFrame):
+            if requested_frame_ids and frame.frame_id not in requested_frame_ids:
+                return False
+
+            if isinstance(frame, ldfparser.LinUnconditionalFrame) and nodes:
+                if frame.publisher.name not in nodes:
+                    return False
+
+            return True
+
+        return set([f.frame_id for f in ldf.frames if is_frame_allowed(f)])
+
     def run(self, args):
         ldf = ldfparser.parse_ldf(path=args.ldf_path)
         pub_width = max(len(ldf.master.name), max([len(p.name) for p in ldf.slaves]))
@@ -37,12 +88,21 @@ class DumpCommand:
         plin.start(mode=PLINMode.SLAVE, baudrate=ldf.get_baudrate())
         plin.set_id_filter(bytearray([0xFF] * 8))
 
-        ts_offset_us = None
+        allowed_frames = self.get_allowed_frames(
+            ldf, args.frame, args.master, args.node
+        )
+        if not allowed_frames:
+            stderr.print("Filter removes all frames!")
+            exit(1)
+        print("Filtered frames:", ", ".join([f"0x{f:02x}" for f in allowed_frames]))
 
+        ts_offset_us = None
         while True:
             try:
                 received = os.read(plin.fd, PLINMessage.buffer_length)
                 received = PLINMessage.from_buffer_copy(received)
+                if received.id not in allowed_frames:
+                    continue
 
                 if not ts_offset_us:
                     ts_offset_us = received.ts_us
